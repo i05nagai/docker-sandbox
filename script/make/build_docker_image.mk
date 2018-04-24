93deaeeff683bd85652caeca3ca85d95787a042e
i05nagai <i05nagai@gmail.com>
2018-04-24 12:31:54 +0900

    Add script for make

---
+++ script/make/build_docker_image.mk
@@ -0,0 +1,23 @@
+DOCKER_IMAGE_PREFIX = gcr.io/project/
+DOCKER_COMMAND = gcloud docker --
+DIRNAME = $(shell basename $(shell pwd))
+NAME = $(subst docker-,,${DIRNAME})
+VERSION ?= latest
+IMAGE = ${DOCKER_IMAGE_PREFIX}${NAME}
+DOCKER_BUILD_ARGS = --build-arg DOCKER_IMAGE_PREFIX=${DOCKER_IMAGE_PREFIX}
+
+build: pre-build docker-build post-build
+
+pre-build:
+
+post-build:
+
+docker-build:
+	docker build -t ${IMAGE}:${VERSION} ${DOCKER_BUILD_ARGS} .
+
+push: docker-push post-push
+
+post-push:
+
+docker-push:
+	${DOCKER_COMMAND} push ${IMAGE}:${VERSION}
