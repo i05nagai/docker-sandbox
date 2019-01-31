845396f1ffcc29edc8ff631da055f84d9e0898cf
i05nagai <i05nagai@gmail.com>
2019-02-01 00:47:55 +0900

    Update docker-flask

--- docker-flask/docker_run.sh
+++ docker-flask/docker_run.sh
@@ -4,5 +4,6 @@ docker run --rm -it \
   --name flask \
   --volume $(pwd)/opt:/tmp/opt \
   --workdir /tmp/opt/flask \
+  -p 8080:5000 \
   i05nagai/flask:latest \
   /bin/bash
--- docker-flask/opt/flask/app/sample.py
+++ docker-flask/opt/flask/app/sample.py
@@ -7,6 +7,21 @@ def index():
     return 'Index Page'
 
 
+@app.route('/error400')
+def error400():
+    flask.abort(400)
+
+
+@app.route('/error500')
+def error500():
+    flask.abort(500)
+
+
+@app.route('/stacktrace')
+def stacktrace():
+    raise ValueError('Stacktrace')
+
+
 def main():
     index()
 
