bc1d4fdb651d6e10d32238081f65c75bcc0e3cbf
i05nagai <i05nagai@gmail.com>
2019-02-03 23:55:35 +0900

    Update docker-nginx

--- docker-nginx/docker_run.sh
+++ docker-nginx/docker_run.sh
@@ -5,7 +5,8 @@ set -x
 docker run --rm \
   -it \
   --name nginx \
+  --volume $HOME/.local/var/log/nginx:/var/log/nginx \
+  --volume $(pwd)/templates/certifications:/etc/ssl:ro \
   -p 8111:80 \
   -p 8443:443 \
-  -v $(pwd)/certifications:/etc/ssl:ro \
   i05nagai/nginx:latest
