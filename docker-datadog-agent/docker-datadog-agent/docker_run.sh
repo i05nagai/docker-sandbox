5d16790dbf2da3f52d94fce370054348078eb0f4
i05nagai <i05nagai@gmail.com>
2019-02-03 23:55:10 +0900

    Update docker-datadog-agent

--- docker-datadog-agent/docker-datadog-agent/docker_run.sh
+++ docker-datadog-agent/docker-datadog-agent/docker_run.sh
@@ -5,8 +5,11 @@ docker run --rm -it \
   --volume /var/run/docker.sock:/var/run/docker.sock:ro \
   --volume /proc/:/host/proc/:ro \
   --volume /sys/fs/cgroup/:/host/sys/fs/cgroup:ro \
+  --volume $HOME/.local/var/log:/host/var/log:ro \
   --env DD_API_KEY=${DATADOG_API_KEY} \
   --env DD_LOGS_ENABLED="true" \
   --env DD_PROCESS_AGENT_ENABLED="true" \
-  --env DD_DOCKER_LABELS_AS_TAGS
+  --env DD_DOCKER_LABELS_AS_TAGS="true" \
+  --env DD_LOGS_CONFIG_CONTAINER_COLLECT_ALL="true" \
+  --env DD_AC_EXCLUDE="name:datadog-agent" \
   i05nagai/datadog-agent:latest
