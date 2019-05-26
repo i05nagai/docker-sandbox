871cbbc40f9d7b1505b21ba1add19f9fe14292e2
i05nagai <i05nagai@gmail.com>
2019-05-26 16:12:39 +0200

    Update docker-openldap

--- docker-openldap/template/opt/local/openldap/openldap_add_users.sh
+++ docker-openldap/template/opt/local/openldap/openldap_add_users.sh
@@ -4,12 +4,14 @@ set -e
 set -x
 
 PATH_THIS_DIR=$(cd $(dirname ${0});pwd)
-PASSWORD_I05NAGAI="${PASSWORD_I05NAGAI:-"$(slappasswd -s i05nagai)"}"
-cat ${PATH_THIS_DIR}/_add_content.ldif > add_content.ldif
+export PASSWORD_I05NAGAI="${PASSWORD_I05NAGAI:-"$(slappasswd -s i05nagai)"}"
+# ' ' is delimiter for sed since password contain '/'
+cat ${PATH_THIS_DIR}/_add_content.ldif | sed "s __PASSWORD_I05NAGAI__ ${PASSWORD_I05NAGAI} " > ${PATH_THIS_DIR}/add_content.ldif
 
 ldapadd \
     -x \
     -D cn=admin,dc=example,dc=com \
-    -W -f add_content.ldif
+    -W \
+    -f ${PATH_THIS_DIR}/add_content.ldif
 
 # ldapadd -x -D cn=i05nagai,dc=example,dc=com -W -f add_content.ldif
--- docker-openldap/template/opt/local/openldap/openldap_modify_users.sh
+++ docker-openldap/template/opt/local/openldap/openldap_modify_users.sh
@@ -4,21 +4,22 @@ set -e
 set -x
 
 PATH_THIS_DIR=$(cd $(dirname ${0});pwd)
-PASSWORD_I05NAGAI="${PASSWORD_I05NAGAI:-"$(slappasswd -s i05nagai)"}"
-cat ${PATH_THIS_DIR}/_modify.ldif > modify.ldif
+export PASSWORD_I05NAGAI="${PASSWORD_I05NAGAI:-"$(slappasswd -s i05nagai)"}"
+# ' ' is delimiter for sed since password contain '/'
+cat ${PATH_THIS_DIR}/_modify.ldif | sed -e "s __PASSWORD_I05NAGAI__ ${PASSWORD_I05NAGAI} " > ${PATH_THIS_DIR}/modify.ldif
 
 # modify as admin
-ldapmodify \
-    -x \
-    -W \
-    -v \
-    -D "cn=admin,dc=example,dc=com" \
-    -f modify.ldif
-
-# modify as i05nagai
 # ldapmodify \
 #     -x \
 #     -W \
 #     -v \
-#     -D "uid=i05nagai,ou=People,dc=example,dc=com" \
-#     -f modify.ldif
+#     -D "cn=admin,dc=example,dc=com" \
+#     -f ${PATH_THIS_DIR}/modify.ldif
+
+# modify as i05nagai
+ldapmodify \
+    -x \
+    -W \
+    -v \
+    -D "uid=i05nagai,ou=People,dc=example,dc=com" \
+    -f ${PATH_THIS_DIR}/modify.ldif
