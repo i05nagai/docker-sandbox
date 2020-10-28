bc71bf889a2b3575428e5a89a10b0e7ca0b69ea0
i05nagai <i05nagai@gmail.com>
2020-10-28 08:55:33 +0100

    Update docker-java8

---
+++ docker-java8/template/opt/local/java8/project1/Project.java
@@ -0,0 +1,8 @@
+package project1;
+
+public class Project {
+
+    public void hello() {
+        System.out.println("Bar: ");
+    }
+}
---
+++ docker-java8/template/opt/local/java8/project1/ProjectTest.java
@@ -0,0 +1,13 @@
+package project1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ProjectTest {
+
+    @Test
+    public void helloTest() {
+      assertEquals(true, true);
+      assertEquals(true, false);
+    }
+}
